7d04744b259cbd9599b0448ed7979b39fdda4133
dmitriy <aleksandrovdmitriy812@gmail.com>
2025-11-23 14:45:38 +0200

    feat: add auth

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *     title="Smart Task Manager API",
+ *     version="1.0.0",
+ *     description="Intelligent task tracker with automatic assignment based on skills and workload"
+ * )
+ * @OA\SecurityScheme(
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="Token"
+ * )
+ */
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
